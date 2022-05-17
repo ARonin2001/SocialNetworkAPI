fe9a33853c8c847aa52ae9b2f89a63e11f5f5b6b
Alex <Aronin.2001@gmail.com>
2022-05-17 23:08:23 +0400

    Get image by name and upload an image

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 require('./app/models');
 const config = require('./config');
 
@@ -14,5 +20,65 @@ mongoose.connect(mongoUri)
         appPort, 
         () => console.log(`Listening on port: ${appPort}...`))   
     )
-    .catch(err => console.error(`Error connection to mongo: ${mongoUri}`, err))
+    .catch(err => console.error(`Error connection to mongo: ${mongoUri}`, err));
+
+const conn = mongoose.createConnection(mongoUri);
+
+let gfs;
+let GridFsBucket;
+
+
+conn.once('open', () => {
+    GridFsBucket = new mongodb.GridFSBucket(conn.db, {
+        bucketName: 'images'
+    });
+
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('images');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+    url: mongoUri,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'images'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  }); 
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('avatar'), (req, res) => {
+    res.json({file: req.file});
+});
+
+app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+        if(!file || file.length === 0) {
+            return res.status(404).json({message: "No file exists"});
+        }
+
+        // Check if image is exist
+        if(file.contentType === "image/jpeg" || file.contentType === "img/png") {
+            // Read output to browser
+            // const readstream = gfs.createReadStream(file.filename);
+            const readstream = GridFsBucket.openDownloadStream(file._id);
+            readstream.pipe(res);
+        } else {
+            res.status(404).json({message: 'Not an (jpep/png) image'})
+        }
+    })
+});
+
+
 
