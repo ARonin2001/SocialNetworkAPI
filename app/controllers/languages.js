1d0cdfd00890be95948afef818d003c5ff58937a
Alex <Aronin.2001@gmail.com>
2022-05-26 23:13:00 +0400

    Languages

--- app/controllers/languages.js
+++ app/controllers/languages.js
@@ -3,7 +3,7 @@ const Language = mongoose.model('Language');
 
 // get all the languages in original name
 const getAllLanguagesInOriginalName = (req, res) => {
-    Language.find({}, {name: 1})
+    Language.find({}, {name: 1, native: 1})
         .exec()
         .then(language => res.status(200).json({languages: language}))
         .catch(err => res.status(404).json({message: err}))
